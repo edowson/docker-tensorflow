c52debf40cec5a9d651773ecba4f8a308f8b92ce
Elvis Dowson <elvis.dowson@gmail.com>
2019-07-10 02:59:18 +0400

    ubuntu/xenial/tf-dev-base: Add docker image.
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

---
+++ ubuntu/xenial/tf-dev-base/build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+BUILD_DATE=$(date -u +'%Y-%m-%d-%H:%M:%S')
+BUILD_VERSION=${1:-"v2.0.0-beta1"}
+ORG='edowson'
+IMAGE='tf-dev-base'
+IMAGE_FEATURE=${2:-"gpu"}
+REPOSITORY="$ORG/$IMAGE/$IMAGE_FEATURE"
+BAZEL_VERSION='0.26.0'
+CUDA_MAJOR_VERSION='10.1'
+CUDNN_VERSION='7.6.1.34'
+CONDA_PYTHON_VERSION='3'
+CONDA_BASE_PACKAGE='anaconda'
+CONDA_VERSION='2019.03'
+LLVM_VERSION='7'
+NVIDIA_DRIVER_VERSION='430.26'
+NCCL2_VERSION='2.4.7'
+VULKAN_SDK_VERSION='1.1.108.0'
+TENSORFLOW_VERSION="$BUILD_VERSION"
+TENSORRT_VERSION='5.1.5.0'
+TF_PYTHON_VERSION=${3:-"3.6"}
+USER='developer'
+USER_ID='1000'
+CODE_NAME='xenial'
+BUILD_IMAGE='tf-build'
+BUILD_OUTPUT="/home/$USER/tensorflow_pkg"
+TAG="$TENSORFLOW_VERSION-$CUDA_MAJOR_VERSION-$CODE_NAME"
+OPTION=""
+
+: '
+Information about available software versions:
+01. anaconda: 2019.03
+02. miniconda: 4.6.14
+03. LLVM: 5.0, 6.0, 7, 8.
+'
+
+# use tar to dereference the symbolic links from the current directory,
+# and then pipe them all to the docker build - command
+tar -czh . | docker build - \
+  --build-arg REPOSITORY=nvidia/cudagl \
+  --build-arg TAG="$CUDA_MAJOR_VERSION-devel-ubuntu16.04" \
+  --build-arg BAZEL_VERSION=$BAZEL_VERSION \
+  --build-arg BUILD_BASE="$ORG/$BUILD_IMAGE/$IMAGE_FEATURE:$TAG" \
+  --build-arg BUILD_OUTPUT=$BUILD_OUTPUT \
+  --build-arg CUDA_MAJOR_VERSION=$CUDA_MAJOR_VERSION \
+  --build-arg CUDNN_VERSION=$CUDNN_VERSION \
+  --build-arg CONDA_PYTHON_VERSION=$CONDA_PYTHON_VERSION \
+  --build-arg CONDA_BASE_PACKAGE=$CONDA_BASE_PACKAGE \
+  --build-arg CONDA_VERSION=$CONDA_VERSION \
+  --build-arg IMAGE_FEATURE=$IMAGE_FEATURE \
+  --build-arg LLVM_VERSION=$LLVM_VERSION \
+  --build-arg NVIDIA_DRIVER_VERSION=$NVIDIA_DRIVER_VERSION \
+  --build-arg NCCL2_VERSION=$NCCL2_VERSION \
+  --build-arg VULKAN_SDK_VERSION=$VULKAN_SDK_VERSION \
+  --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION \
+  --build-arg TENSORRT_VERSION=$TENSORRT_VERSION \
+  --build-arg TF_PYTHON_VERSION=$TF_PYTHON_VERSION \
+  --build-arg BUILD_VERSION=$BUILD_VERSION \
+  --build-arg BUILD_DATE=$BUILD_DATE \
+  --build-arg USER=$USER \
+  --build-arg UID=$USER_ID \
+  --tag=${REPOSITORY}:${TAG}
---
+++ ubuntu/xenial/tf-dev-base/readme.md
@@ -0,0 +1,65 @@
+# Ubuntu-16.04 LTS TensorFlow development base docker image
+
+## Overview
+
+This docker image creates a TensorFlow v2.0 development base image with support for Bazel, LLVM, NVIDIA CUDA, TensorRT and Vulkan SDK.
+
+This docker image has been tested on an Ubuntu-18.04 LTS host running:
+- NVIDIA Docker 2
+- NVIDIA Driver 430.26
+
+This [Dockerfile](./Dockerfile) depends upon the [TensorFlow - Build - Ubuntu-16.04](//ubuntu/xenial/tf-build) docker image.
+
+## Usage
+
+Build the docker image
+```bash
+cd ubuntu/xenial/tf-base
+./build.sh
+```
+
+Run the docker image:
+```bash
+./run.sh
+```
+
+Activate tensorflow conda environment:
+```bash
+conda activate tf3.6
+python --version
+Python 3.6.8 :: Anaconda, Inc.
+```
+
+Run a matrix multiply test:
+```bash
+nano matmul.py
+```
+
+```py
+"""
+Credits: Author: Dr. Donald Kinghorn, Puget Systems.
+"""
+import tensorflow as tf
+import time
+tf.set_random_seed(42)
+A = tf.random_normal([10000,10000])
+B = tf.random_normal([10000,10000])
+def checkMM():
+    start_time = time.time()
+    with tf.Session() as sess:
+            print( sess.run( tf.reduce_sum( tf.matmul(A,B) ) ) )
+    print(" took {} seconds".format(time.time() - start_time))
+
+checkMM()
+```
+
+```bash
+python matmul.py
+
+-873834.5
+ took 1.8854620456695557 seconds
+```
+
+## Technotes
+
+[Build TensorFlow-CPU with MKL and Anaconda Python 3.6 using a Docker Container - Dr Donald Kinghorn - Puget Systems 20180406](https://www.pugetsystems.com/labs/hpc/Build-TensorFlow-CPU-with-MKL-and-Anaconda-Python-3-6-using-a-Docker-Container-1133/)
