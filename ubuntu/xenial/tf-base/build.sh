143116f12237563ee877e6aea616ccdb82a2bf7e
Elvis Dowson <elvis.dowson@gmail.com>
2019-07-10 02:58:52 +0400

    ubuntu/xenial/tf-base: Add docker image.
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

---
+++ ubuntu/xenial/tf-base/build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+BUILD_DATE=$(date -u +'%Y-%m-%d-%H:%M:%S')
+BUILD_VERSION=${1:-"v2.0.0-beta1"}
+ORG='edowson'
+IMAGE='tf-base'
+IMAGE_FEATURE=${2:-"gpu"}
+REPOSITORY="$ORG/$IMAGE/$IMAGE_FEATURE"
+CUDA_MAJOR_VERSION='10.1'
+CUDNN_VERSION='7.6.1.34'
+CONDA_PYTHON_VERSION='3'
+CONDA_BASE_PACKAGE='miniconda'
+CONDA_VERSION='4.6.14'
+NCCL2_VERSION='2.4.7'
+TENSORFLOW_VERSION="$BUILD_VERSION"
+TENSORRT_VERSION='5.1.5.0'
+TF_PYTHON_VERSION=${3:-"3.6"}
+USER='developer'
+USER_ID='1000'
+CODE_NAME='xenial'
+BUILD_IMAGE='tf-build'
+BUILD_OUTPUT="/home/$USER/tensorflow_pkg"
+TAG="$TENSORFLOW_VERSION-$CUDA_MAJOR_VERSION-$CODE_NAME"
+OPTION=""
+
+: '
+Information about available software versions:
+01. anaconda: 2019.03
+02. miniconda: 4.6.14
+'
+
+# use tar to dereference the symbolic links from the current directory,
+# and then pipe them all to the docker build - command
+tar -czh . | docker build - \
+  --build-arg REPOSITORY=nvidia/cudagl \
+  --build-arg TAG="$CUDA_MAJOR_VERSION-devel-ubuntu16.04" \
+  --build-arg BUILD_BASE="$ORG/$BUILD_IMAGE/$IMAGE_FEATURE:$TAG" \
+  --build-arg BUILD_OUTPUT=$BUILD_OUTPUT \
+  --build-arg CUDA_MAJOR_VERSION=$CUDA_MAJOR_VERSION \
+  --build-arg CUDNN_VERSION=$CUDNN_VERSION \
+  --build-arg CONDA_PYTHON_VERSION=$CONDA_PYTHON_VERSION \
+  --build-arg CONDA_BASE_PACKAGE=$CONDA_BASE_PACKAGE \
+  --build-arg CONDA_VERSION=$CONDA_VERSION \
+  --build-arg IMAGE_FEATURE=$IMAGE_FEATURE \
+  --build-arg NCCL2_VERSION=$NCCL2_VERSION \
+  --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION \
+  --build-arg TENSORRT_VERSION=$TENSORRT_VERSION \
+  --build-arg TF_PYTHON_VERSION=$TF_PYTHON_VERSION \
+  --build-arg BUILD_VERSION=$BUILD_VERSION \
+  --build-arg BUILD_DATE=$BUILD_DATE \
+  --build-arg USER=$USER \
+  --build-arg UID=$USER_ID \
+  --tag=${REPOSITORY}:${TAG}
---
+++ ubuntu/xenial/tf-base/run.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# common variables
+CURRENT_DIR=`pwd`
+SCRIPT_DIR=`dirname $0`
+
+# script variables
+HOST_IP=`hostname -I | awk '{print $1}'`
+DOCKER_DAEMON_IP=`ip -4 -o a | grep docker0 | awk '{print $4}'`
+BUILD_VERSION=${1:-"v2.0.0-beta1"}
+ORG='edowson'
+IMAGE='tf-base'
+IMAGE_FEATURE='gpu'
+REPOSITORY="$ORG/$IMAGE/$IMAGE_FEATURE"
+CUDA_MAJOR_VERSION='10.1'
+CUDNN_VERSION='7.6.1.34'
+CONDA_PYTHON_VERSION='3'
+CONDA_BASE_PACKAGE='anaconda'
+CONDA_VERSION='2019.03'
+NCCL2_VERSION='2.4.7'
+TENSORFLOW_VERSION="$BUILD_VERSION"
+TENSORRT_VERSION='5.1.5.0'
+TF_PYTHON_VERSION=${3:-"3.6"}
+USER='developer'
+USER_ID='1000'
+CODE_NAME='xenial'
+TAG="$TENSORFLOW_VERSION-$CUDA_MAJOR_VERSION-$CODE_NAME"
+OPTION=""
+
+# setup pulseaudio cookie
+if [ x"$(pax11publish -d)" = x ]; then
+    start-pulseaudio-x11;
+    echo `pax11publish -d | grep --color=never -Po '(?<=^Cookie: ).*'`
+fi
+
+# find a free port
+read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+while : ; do
+  PULSE_PORT="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
+  ss -lpn | grep -q ":$PULSE_PORT " || break
+done
+echo "Using PULSE_PORT=$PULSE_PORT"
+
+# load pulseaudio tcp module
+PULSE_MODULE_ID=$(pactl load-module module-native-protocol-tcp port=$PULSE_PORT)
+echo "PULSE_MODULE_ID=$PULSE_MODULE_ID"
+
+# run container
+xhost +local:root
+docker run -it \
+  --runtime=nvidia \
+  --device /dev/snd \
+  -e DISPLAY \
+  -e PULSE_SERVER="tcp:$HOST_IP:$PULSE_PORT" \
+  -e PULSE_COOKIE_DATA=`pax11publish -d | grep --color=never -Po '(?<=^Cookie: ).*'` \
+  -e QT_GRAPHICSSYSTEM=native \
+  -e QT_X11_NO_MITSHM=1 \
+  -e NCCL_VERSION="$NCCL2_VERSION" \
+  -v /dev/shm:/dev/shm \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket:ro \
+  -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/1000/pulse/native \
+  -v ~/mount/backup:/backup \
+  -v ~/mount/data:/data \
+  -v ~/mount/project:/project \
+  -v ~/mount/tool:/tool \
+  --rm \
+  --name ${IMAGE}-${TAG} \
+  ${REPOSITORY}:${TAG} bash
+
+xhost -local:root
+
+# unload pulseaudio module
+pactl unload-module $PULSE_MODULE_ID
